d2eca832f033fea3632b121efe4fd1751d6a9f7c
shvec-es <kotova.es.1510@gmail.com>
2022-01-26 11:11:06 +0200

    Updates

---
+++ static/js/main.db4bd7f9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpproject-vseti"]=this["webpackJsonpproject-vseti"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(3),r=n.n(c),s=(n(8),n(1),n(0));var i=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"\u0417\u0434\u0435\u0441\u044c \u0431\u0443\u0434\u0435\u0442 \u043d\u0430\u0448 \u043f\u0440\u043e\u0435\u043a\u0442!"})})};r.a.render(Object(s.jsx)(i,{}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.db4bd7f9.chunk.js.map
